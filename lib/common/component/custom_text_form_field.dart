2c25d778b026dd035c41c6c5d8a16a0eaba61d3c
hansookim <suekhan@naver.com>
2024-12-14 13:47:24 +0900

    Initial commit

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,52 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool authFocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    required this.onChanged,
+    this.authFocus = false,
+    this.obscureText = false,
+    this.errorText,
+    this.hintText,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력
+      obscureText: obscureText,
+      autofocus: authFocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(color: PRIMARY_COLOR),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:actual/common/component/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요',
+              onChanged: (value) {},
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요',
+              obscureText: true,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
