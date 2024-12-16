bf1cb097e216f04cce77c5aaf6f4ffa1a321d330
hansookim <suekhan@naver.com>
2024-12-16 23:52:11 +0900

    2024.12.16

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/component/custom_text_form_field.dart';
+import 'package:actual/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,23 +12,13 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextFormField(
-              hintText: '이메일을 입력해주세요',
-              onChanged: (value) {},
-            ),
-            CustomTextFormField(
-              hintText: '비밀번호를 입력해주세요',
-              obscureText: true,
-              onChanged: (value) {},
-            ),
-          ],
-        ),
+        body: LoginScreen(),
       ),
     );
   }
